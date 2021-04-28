00e038f0c9500e5a4adee1b870d00e89b049c42d
sbyte <stanko.bajt@gmail.com>
2021-04-28 09:49:23 +0200

    Fixes text in meeting view holder when meeting lasts all day.

--- app/src/main/java/com/scorealarm/meeting/rooms/list/viewholders/MeetingViewHolder.kt
+++ app/src/main/java/com/scorealarm/meeting/rooms/list/viewholders/MeetingViewHolder.kt
@@ -1,12 +1,11 @@
 package com.scorealarm.meeting.rooms.list.viewholders
 
 import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.scorealarm.meeting.rooms.R
 import com.scorealarm.meeting.rooms.models.Meeting
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_meeting_list.*
+import org.joda.time.Period
 
 class MeetingViewHolder(
     override val containerView: View,
@@ -16,7 +15,12 @@ class MeetingViewHolder(
 
     fun bind(meeting: Meeting) {
         meeting.run {
-            timeView?.text = "${startDateTime.toString("HH:mm")} - ${endDateTime.toString("HH:mm")}"
+            timeView?.text = if (Period(
+                    meeting.startDateTime,
+                    meeting.endDateTime
+                ).days == 1
+            ) "Meeting lasts all day"
+            else "${meeting.startDateTime.toString("HH:mm")} - ${meeting.endDateTime.toString("HH:mm")}"
             meetingNameView?.text = title
             meetingOrganizerView?.text = organizer
             attendeeCountView?.text = invitesNumber.toString()
